23c7866b76e42a6da35e1bfec097c9d890c89b5a
JuradoBruno <jurado.bruno@gmail.com>
2019-09-21 00:38:49 +0200

    Moving truck is working

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,6 +1,9 @@
 import { Component } from '@angular/core';
 import * as L from 'leaflet';
 import MiniMap from 'leaflet-minimap';
+import 'leaflet-routing-machine';
+import "../services/AnimatedMarker.js"
+import route01 from "../services/route01.json"
 
 @Component({
   selector: 'app-home',
@@ -8,12 +11,30 @@ import MiniMap from 'leaflet-minimap';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-
+  
   map
   osmStreetMap = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
   osmWiki = 'http://maps.wikimedia.org/osm-intl/{z}/{x}/{y}.png'
   osmAttribution = '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 
+  disableMissionButton = false
+
+  redAmbuIcon = {
+    icon: L.icon({
+    iconSize: [90, 50],
+    iconAnchor: [35, 41], // - margin-top, -margin-left
+    iconUrl: 'assets/icon/ambulance-side-red.png',
+    className: 'ambulance-side'
+    }
+  )}
+
+  hopitalIcon = {
+    icon: L.icon({
+    iconSize: [80, 80],
+    iconAnchor: [35, 41], // - margin-top, -margin-left
+    iconUrl: 'assets/icon/hopital-rond.png'
+    }
+  )}
   // Define our base layers so we can reference them multiple times
   streetMaps = L.tileLayer(this.osmStreetMap, {
     detectRetina: true,
@@ -24,43 +45,10 @@ export class HomePage {
     attribution: this.osmAttribution
   });
 
-
-  // greenIcon = L.icon({
-  //   iconUrl: 'leaf-green.png',
-  //   shadowUrl: 'leaf-shadow.png',
-
-  //   iconSize:     [38, 95], // size of the icon
-  //   shadowSize:   [50, 64], // size of the shadow
-  //   iconAnchor:   [22, 94], // point of the icon which will correspond to marker's location
-  //   shadowAnchor: [4, 62],  // the same for the shadow
-  //   popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
-  // });
-
-  // Leaflet Marker
-  greenLeaf = L.marker([46.8523, -121.7603], {
-    icon: L.icon({
-      iconUrl: 'assets/icon/leaf-green.png',
-      shadowUrl: 'assets/icon/leaf-shadow.png',
-
-      iconSize:     [38, 95], // size of the icon
-      shadowSize:   [50, 64], // size of the shadow
-      iconAnchor:   [22, 94], // point of the icon which will correspond to marker's location
-      shadowAnchor: [4, 62],  // the same for the shadow
-      popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
-    })
-  });
-
   // Marker for the parking lot at the base of Mt. Ranier trails
-  icon = L.marker([43.609598, 1.401405], {
-    icon: L.icon({
-      iconSize: [90, 50],
-      iconAnchor: [35, 41], // - margin-top, -margin-left
-      iconUrl: 'assets/icon/ambulance-side-red.png',
-      className: 'ambulance-side'
-    })
-  });
-
-  // Path from icon to summit - most points omitted from this example for brevity
+  redAmbu = L.marker([43.609598, 1.401405], this.redAmbuIcon);
+  hopital1 = L.marker([43.609598, 1.401405], this.hopitalIcon);
+  hopital2 = L.marker([43.601380, 1.433971], this.hopitalIcon);
 
   // Layers control object with our two base layers and the three overlay layers
   layersControl = {
@@ -69,13 +57,13 @@ export class HomePage {
       'Wikimedia Maps': this.wMaps
     },
     overlays: {
-      'Mt. Rainier icon Start': this.icon,
+      'Red Ambulance': this.redAmbu,
     }
   };
 
   ////// MAIN MAP OPTIONS ///////
   mainMapOptions = {
-    layers: [this.streetMaps, this.icon, this.greenLeaf],
+    layers: [this.streetMaps, this.hopital1, this.hopital2],
     zoom: 16,
     center: L.latLng([43.609598, 1.401405])
   };
@@ -96,11 +84,46 @@ export class HomePage {
   }
 
   onMapReady() {
-    this.addMinimap()    
+    this.addMinimap()
+    // this.addRouting()
   }
 
   addMinimap() {
     let osm = new L.TileLayer(this.osmStreetMap)
     let miniMap = new MiniMap(osm, { minZoom: 0, maxZoom: 13, attribution: this.osmAttribution }).addTo(this.map)
   }
+
+  addRouting() {
+    let control = L.Routing.control({
+      waypoints: [
+          L.latLng(43.609598, 1.401405),
+          L.latLng(43.601380, 1.433971)
+      ],
+      // router: new L.Routing.OSRMv1({
+      //   serviceUrl: 'http://router.project-osrm.org/'
+      // }),
+    }).addTo(this.map);
+
+    control.on('routeselected', event => {
+      let coords = event.route.coordinates;
+      let instr = event.route.instructions;
+      this.getTheTruckMoving(coords)
+    });
+  }
+
+  getTheTruckMoving(coords) {
+    this.disableMissionButton = true
+    coords = route01
+    var line = L.polyline(coords),
+    animatedMarker = L.animatedMarker(line.getLatLngs(), {
+      icon: this.redAmbuIcon.icon,
+      distance: 5000, // meters
+      interval: 200, // milliseconds,      
+    });
+
+    this.map.addLayer(animatedMarker);
+  }
+
+
+
 }
