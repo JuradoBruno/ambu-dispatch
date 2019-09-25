a87dc5c132e2f62024095472c5be85e132043885
JuradoBruno <jurado.bruno@gmail.com>
2019-09-25 20:56:54 +0200

    Hot module reload installed

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,8 @@
 
 export const environment = {
   production: false,
-  baseUrl: 'http://localhost:3000/auth/signup'
+  baseUrl: 'ambu-dispatch-recette.eu-west-1.elasticbeanstalk.com',
+  hmr: false
 };
 
 /*
