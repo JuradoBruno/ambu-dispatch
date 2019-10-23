9feac1d7ebc5ed2474debaf3fe3e49fcdef6e321
JuradoBruno <jurado.bruno@gmail.com>
2019-10-23 13:41:11 +0200

    Refacto base-hhtp service

--- src/app/core/services/base-http.service.ts
+++ src/app/core/services/base-http.service.ts
@@ -1,25 +1,33 @@
 import { Injectable } from '@angular/core';
 import { ObservableStore } from '@codewithdan/observable-store';
 import { IStoreState } from 'src/app/core/stores/store-state.interface';
-import { HttpHeaders } from '@angular/common/http';
+import { HttpHeaders, HttpClient } from '@angular/common/http';
+import { environment } from '../../../environments/environment';
 
 @Injectable()
 export class BaseHttpService extends ObservableStore<IStoreState> {
+    baseUrl = environment.baseUrl
 
     initialState: IBaseHttpStore = {
         accessToken: null,
     }
 
-    constructor() {
-        super({
-            // stateSliceSelector: state => {
-            //     return {
-            //         accessToken: state.accessToken
-            //     }
-            // }
-        })
+    constructor(
+        private http: HttpClient
+    ) {
+        super({})
         this.setState(this.initialState, BaseHttpActions.InitState)
     }
+
+    get<T>(url, options = {}): Promise<T> {
+        return this.http.get<T>(this.baseUrl + url, options).toPromise()
+    }
+
+    post<T>(url, body, options = {}): Promise<T | any> {
+        let headers = this.returnHeaders()
+        return this.http.post<T>(this.baseUrl + url, body, {...options, headers}).toPromise()
+    }
+
 
     saveToken(accessToken) {
         localStorage.setItem('accessToken', accessToken);
